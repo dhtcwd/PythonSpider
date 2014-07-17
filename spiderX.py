0f695507d6d691c2627438e7245bab2a2eb89a16
Will <dhtcwd@qq.com>
2014-07-17 11:40:42 +0800

    download normal pic

--- spiderX.py
+++ spiderX.py
@@ -1,6 +1,8 @@
 #coding=utf-8
 import urllib
 import re
+import datetime
+import time
  
 def getHtml(url):
     page = urllib.urlopen(url)
@@ -8,18 +10,20 @@ def getHtml(url):
     return html
  
 def getImg(html):
-    reg = r'src="(.+?\.jpg)"'
+    reg = r'src="(.+thumb.+\.jpg)"'
     imgre = re.compile(reg)
     imglist = re.findall(imgre,html)
-    x = 0
     for imgurl in imglist:
         strinfo = re.compile('thumb')
-        imgurl = strinfo.sub('large',imgurl)
-        print imgurl
+        imgurl = strinfo.sub('photo',imgurl)
+        x = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))+str(datetime.datetime.now().microsecond)
         local = 'E://myimage//'
         urllib.urlretrieve(imgurl,local+'%s.jpg' % x)
-        x+=1
 
-html = getHtml("http://www.douban.com/photos/album/63888587/")
- 
-print getImg(html)
+albumId = raw_input('please enter the albumId: ')
+paper = int(raw_input('please enter the num of paper: '))
+for x in range(paper):
+    index = str(x * 18)
+    albumUrl = "http://www.douban.com/photos/album/"+albumId+"/?start="+index
+    html = getHtml(albumUrl)
+    getImg(html)
