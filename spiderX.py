72da7635fe83e1a2b0c18941c7006ba6932515e1
Will <dhtcwd@qq.com>
2014-07-18 17:23:25 +0800

    0.20

--- spiderX.py
+++ spiderX.py
@@ -1,16 +1,35 @@
-#coding=utf-8
+# -*- coding=utf-8 -*-
+
+#---------------------------------------  
+#   程序：豆瓣相册爬虫
+#   版本：0.2  
+#   作者：Will  
+#   日期：2014-07-17  
+#   语言：Python 2.7  
+#   功能：将相册中照片全部抓下来（有大图优先抓大图——比0.1版本的改进)  
+#---------------------------------------  
+
 import urllib
 import re
 import datetime
 import time
- 
+
+
+def getPicHtml(html):
+    reg = r'href="(http://www.douban.com/photos/photo.+\/)"'
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre,html)
+    for imgurl in imglist:
+        newHtml = getHtml(imgurl)
+        getImg(newHtml)
+
 def getHtml(url):
     page = urllib.urlopen(url)
     html = page.read()
     return html
  
-def getImg(html):
-    reg = r'src="(.+thumb.+\.jpg)"'
+def getImg(imgurl):
+    reg = r'src="(.+public.+\.jpg)"'
     imgre = re.compile(reg)
     imglist = re.findall(imgre,html)
     for imgurl in imglist:
@@ -26,4 +45,4 @@ for x in range(paper):
     index = str(x * 18)
     albumUrl = "http://www.douban.com/photos/album/"+albumId+"/?start="+index
     html = getHtml(albumUrl)
-    getImg(html)
+    getPicHtml(html);
